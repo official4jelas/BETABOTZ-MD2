6325a73e79ce2676b8015c9d03a384880b3cd4bd
official4jelas <ydiskyha@gmail.com>
2024-06-04 20:07:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ global.author = 'Bot WhatsApp'
 global.maxwarn = '2' // Peringatan maksimum
 
 //INI WAJIB DI ISI!//
-global.lann = 'YOUR_APIKEY_HERE' 
+global.lann = 'oD4wgh58' 
 //Daftar terlebih dahulu https://api.betabotz.eu.org
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
