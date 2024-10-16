0d2e1050c41880db8739fbe2d3464704da6112c5
official4jelas <ydiskyha@gmail.com>
2024-10-17 04:25:50 +0700

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,24 +1,26 @@
-const fetch = require("node-fetch")
-const util = require("util")
+let { igdl } = require('btch-downloader')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
-    m.reply(wait)
-     const url = args[0];
-     let re = await fetch(`https://api.betabotz.eu.org/api/download/igdowloader?url=${url}&apikey=${lann}`)
-     let message = await re.json()  
-    try {             
-        for (let i of message.message ) {
-            conn.sendFile(m.chat, i._url, null, `*Instagram Downloader*`, m)
+    if (!args[0]) throw `🔗 Masukkan Link Instagram-nya, bro!`
+    
+    try {
+        let res = await igdl(args[0])
+        if (!res.length) throw `😢 Gak ada video yang bisa di-download!`
+
+        for (let i of res) {
+            await conn.sendFile(m.chat, i.url, 'instagram.mp4', `🎥 Nih, video dari Instagram:`, m)
+            await conn.delay(1500) // Biar gak terlalu cepat
         }
-    } catch(err) {
-        m.reply(`${eror}`)
+    } catch (error) {
+        throw `⚠️ Error: ${error.message}`
     }
 }
 
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(Instagram|ig|igdl|igstory)$/i
+
+handler.command = /^(ig(dl)?)$/i
+
 handler.limit = true
 
-module.exports = handler
+module.exports = handler;
